ea21adc7c601d5e9381b67874fa8ea00a5effaf8
Oluwagbade Oluwamayowa <31127611+mykhaelmayor@users.noreply.github.com>
2022-04-20 15:07:52 +0100

    commit with toast message and better logic

--- app/src/main/java/com/example/diceroll/MainActivity.kt
+++ app/src/main/java/com/example/diceroll/MainActivity.kt
@@ -19,7 +19,7 @@ class MainActivity : AppCompatActivity() {
 
     private fun rollDice() {
 //        Create new dice object with 6 sides and roll it
-        val dice = Dice (6)
+        val dice = Dice(6)
         val diceRoll = dice.roll()
         val image1: ImageView = findViewById(R.id.diceImage1)
         val image2: ImageView = findViewById(R.id.diceImage2)
@@ -31,45 +31,48 @@ class MainActivity : AppCompatActivity() {
             3 -> R.drawable.dice_3
             4 -> R.drawable.dice_4
             5 -> R.drawable.dice_5
-            else -> R.drawable.dice_6 }
+            else -> R.drawable.dice_6
+        }
         image1.setImageResource(drawableResource)
 
-        val dice2 = Dice (6)
+        val dice2 = Dice(6)
         val drawableResource2 = when (dice2.roll()) {
             1 -> R.drawable.dice_1
             2 -> R.drawable.dice_2
             3 -> R.drawable.dice_3
             4 -> R.drawable.dice_4
             5 -> R.drawable.dice_5
-            else -> R.drawable.dice_6 }
+            else -> R.drawable.dice_6
+        }
         image2.setImageResource(drawableResource2)
 
-        val dice3 = Dice (6)
+        val dice3 = Dice(6)
         val drawableResource3 = when (dice3.roll()) {
             1 -> R.drawable.dice_1
             2 -> R.drawable.dice_2
             3 -> R.drawable.dice_3
             4 -> R.drawable.dice_4
             5 -> R.drawable.dice_5
-            else -> R.drawable.dice_6 }
+            else -> R.drawable.dice_6
+        }
         image3.setImageResource(drawableResource3)
 
+        //creates toast message that shows “You Win” when at least 2 dice throw a 6 each
         if (diceRoll == 6 && dice2.roll() == 6 ||
             diceRoll == 6 && dice3.roll() == 6 ||
             dice2.roll() == 6 && dice3.roll() == 6
         ) {
             Toast.makeText(this, "You Win", Toast.LENGTH_SHORT).show()
-        } else if (diceRoll == 5 && dice2.roll() == 4 ||
-            diceRoll == 4 && dice2.roll() == 5 ||
-            dice2.roll() == 4 && dice3.roll() == 5 ||
-            dice2.roll() == 5 && dice3.roll() == 4 ||
-            dice3.roll() == 4 && diceRoll == 5 ||
-            dice3.roll() == 5 && diceRoll == 4
+
+            //creates toast message that shows “You lose” when at least 2 dice throw a total sum of 9
+        } else if (diceRoll + dice2.roll() == 9 ||
+            diceRoll + dice3.roll() == 9 ||
+            dice2.roll() + dice3.roll() == 9
         ) {
-            Toast.makeText(this, "You Lose" , Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, "You Lose", Toast.LENGTH_SHORT).show()
 
         }
-        }
+    }
 
     class Dice(private val sides: Int) {
 
